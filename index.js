23d315b8936fc1342012291c614ec12eed2f5184
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-19 21:24:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const mapToNegativize = arr => {
+  let newArr = [];
+  arr.forEach( e => newArr.push(e * -1));
+  return newArr;
+};
+
+const mapToNoChange = arr => {
+  let newArr = [];
+  arr.forEach( e => newArr.push(e));
+  return newArr;
+};
+
+
